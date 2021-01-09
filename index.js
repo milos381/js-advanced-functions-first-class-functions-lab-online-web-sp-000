979bbf956a240c66c97c903bfaa2dd5d2f6714b3
milos381 <milos_popovic@outlook.com>
2021-01-09 15:10:01 -0500

    commit tree fa8b9d9a3a39f70998c1c95d80d90264e3ee81fb

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (array) {
+    return array.slice(0,2)
+}
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+};
+const selectingDrivers = [
+    returnFirstTwoDrivers, 
+    returnLastTwoDrivers
+];
+const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+        if (multiplyValue === 4) {
+            return multiplyValue * 4;
+        } else {
+            return multiplyValue * value;
+        }
+      
+    };
+  };
+
+  const fareDoubler = createFareMultiplier(2);
+
+  const fareTripler = createFareMultiplier(3);
+  
+  const selectDifferentDrivers = function (drivers, whichDrivers) {
+    return whichDrivers(drivers);
+  };
